939bc67d9dbce81c72e558156c74b19e57cd36ea
1ikuszak <luke.glica@gmail.com>
2024-08-22 19:00:23 +0200

    fix: (jg-tasks) more slides

--- app/jg-tasks/page.tsx
+++ app/jg-tasks/page.tsx
@@ -43,6 +43,21 @@ export default function JgTasks() {
                 <DynamicImage url="https://lukaszglica-portfolio.s3.eu-north-1.amazonaws.com/jg-tasks-5.png" />
               </div>
             </CarouselItem>
+            <CarouselItem>
+              <div className="relative aspect-video h-full w-full">
+                <DynamicImage url="https://lukaszglica-portfolio.s3.eu-north-1.amazonaws.com/jg-tasks-6.png" />
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="relative aspect-video h-full w-full">
+                <DynamicImage url="https://lukaszglica-portfolio.s3.eu-north-1.amazonaws.com/jg-tasks-7.png" />
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="relative aspect-video h-full w-full">
+                <DynamicImage url="https://lukaszglica-portfolio.s3.eu-north-1.amazonaws.com/jg-tasks-8.png" />
+              </div>
+            </CarouselItem>
           </CarouselContent>
           <CarouselPrevious className="bg-white hover:bg-transparent" />
           <CarouselNext className="bg-white hover:bg-transparent" />
